5753e38e9f6d84217019e3d7f4c5dd20ae10a50b
Igor Ferreira <142438559+IgorF3rreira@users.noreply.github.com>
2023-11-27 22:57:10 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,26 @@
-$('.carousel').carousel({
-    interval: 2000
-  })
+
+//Inserir dados
+localStorage.setItem("produto", "mouse Red Dragon");
+
+// restart sem perder os dados
+
+
+// resgatar item
+const produto = localStorage.getItem("produto");
+
+console.log(produto);
+
+// resgate de item que não existe
+const lastName = localStorage.getItem("lastname");
+
+console.log(lastName);
+
+if(!lastName){
+  console.log("sem sobrenome");
+}
+
+// remover item 
+localStorage.removeItem("produto");
+
+// limpar todos os itens
+localStorage.clear();
